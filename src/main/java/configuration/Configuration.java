6f9fca94d6537ebed4d37ae667cad31babe0dc0d
Jose Bellver <jbellver@dsic.upv.es>
2009-07-22 13:05:52 +0000

    Añadir fichero de properties y carga de los valores del properties.

--- src/main/java/configuration/Configuration.java
+++ src/main/java/configuration/Configuration.java
@@ -1,10 +1,57 @@
 package configuration;
 
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+
+
 public class Configuration {
-	public String serverName = "localhost";
-	public String databaseName = "thomas";
-	public String userName = "thomas";
-	public String password = "thomas";
+	
+	
+	
+	public String serverName;
+	public String databaseName;
+	public String userName;
+	public String password;
+	
+	public Configuration()
+	{
+	
+	
+	//Cargamos los valores desde un archivo .xml 
+	Properties properties = new Properties();
+	
+	
+	  try {
+		   properties.loadFromXML(Configuration.class.getResourceAsStream("/"+"THOMASDemoConfiguration.xml"));
+			for (Enumeration e = properties.keys(); e.hasMoreElements() ; ) {
+			    // Obtenemos el objeto
+			    Object obj = e.nextElement();
+			    if (obj.toString().equalsIgnoreCase("serverName"))
+			    {
+			    	serverName= properties.getProperty(obj.toString());	
+			    }
+			    else if (obj.toString().equalsIgnoreCase("databaseName"))
+			    {
+			    	databaseName= properties.getProperty(obj.toString());
+			    }
+			    else    if (obj.toString().equalsIgnoreCase("userName"))
+			    {
+			    	userName= properties.getProperty(obj.toString());
+			    }
+			    else    if (obj.toString().equalsIgnoreCase("password"))
+			    {
+			    	password= properties.getProperty(obj.toString());
+			    }
+			}
+
+	    } catch (IOException e) {
+	    	System.out.print(e);
+	    }
+	}
 
 	
 }
+
+	
