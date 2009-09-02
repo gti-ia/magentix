2fa7bbc9c4173d46c033b263073c78bbea771f4c
Ricard Lopez <rilopez@dsic.upv.es>
2009-09-02 12:55:34 +0000

    Batería de benchmarks: Benchmark1 realizado por Ricard López

---
+++ src/test/java/benchmarks/bench1/Receptor.java
@@ -0,0 +1,29 @@
+package benchmarks.bench1;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import org.apache.qpid.transport.Connection;
+import org.apache.qpid.transport.MessageTransfer;
+
+import es.upv.dsic.gti_ia.fipa.ACLMessage;
+import es.upv.dsic.gti_ia.fipa.AgentID;
+import es.upv.dsic.gti_ia.magentix2.SingleAgent;
+
+public class Receptor extends SingleAgent{
+	
+	LinkedBlockingQueue<MessageTransfer> internalQueue;
+	
+	public Receptor(AgentID aid, Connection connection) {
+		super(aid, connection);
+	}
+	
+	public void execute(){		
+		while(true)
+		{
+			ACLMessage msg = receiveACLMessage();	//esperem missatge des de'l emisor
+			AgentID sender = msg.getReceiver();		//tornem el missatge al emisor
+			msg.setReceiver(msg.getSender());
+			msg.setSender(sender);
+			send(msg);
+		}
+	}
+}
