2aff155f6a0e93725512e592f320f00289b99398
Luis Burdalo <lburdalo@dsic.upv.es>
2010-05-04 15:52:55 +0000

    Created a new basic example in the test folder Trace_ProdCons.

---
+++ magentix2/src/test/java/Trace_ProdCons/ConsumerAgent.java
@@ -0,0 +1,75 @@
+package Trace_ProdCons;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import org.apache.qpid.transport.MessageTransfer;
+
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.BaseAgent;
+import es.upv.dsic.gti_ia.core.TraceEvent;
+
+import es.upv.dsic.gti_ia.trace.*;
+
+/**
+ * ConsumerAgent class defines the structure of a consumer BaseAgent
+ * 
+ * Subscribes to the event_type "TRACE_TEST" and waits for 10 seconds for events to arrive.
+ * When a TRACE_TEST event arrives, ConsumerAgent prints its content on the screen.
+ * After the 10 seconds, ConsumerAgent unsubscribes from the event type and then ends its execution
+ * 
+ * @author Luis Burdalo - lburdalo@dsic.upv.es
+ */
+public class ConsumerAgent extends BaseAgent {
+
+	LinkedBlockingQueue<MessageTransfer> internalQueue;
+
+	public ConsumerAgent(AgentID aid) throws Exception {
+		super(aid);
+	}
+
+	public void execute() {
+		logger.info("[CONSUMER " + getName() +"]: Executing...");
+		/**
+		 * This agent has no definite work. Wait infinitely the arrival of new
+		 * messages.
+		 */
+		
+		// Subscribe to trace events of the type "TRACE_TEST", coming from any tracing entity
+		TraceInteract.requestTracingService(this, "TRACE_TEST");
+    	
+    	for (int i=0; i < 10; i++) {
+			try {
+				logger.info("[CONSUMER " + getName() + "]: Waiting (" + (i+1) + ")...");
+				Thread.currentThread().sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+    	
+    	// Unsubscribe to trace events of the type "TRACE_TEST", coming from any tracing entity
+    	TraceInteract.cancelTracingServiceSubscription(this, "TRACE_TEST");
+		
+    	try {
+			Thread.currentThread().sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		logger.info("[CONSUMER " + getName() + "]: Bye!");
+		
+	}
+
+	public void onTraceEvent(TraceEvent tEvent) {
+		/**
+		 * When a trace event arrives, its shows it on the screen
+		 */
+		logger.info("[CONSUMER " + getName() +"]: Trace event received by onTraceEvent: " + tEvent.toReadableString());
+	}
+	
+	public void onMessage(ACLMessage msg){
+		logger.info("[CONSUMER " + getName() +"]: Message received by onMessage: " + msg.getContent());
+	}
+
+}
---
+++ magentix2/src/test/java/Trace_ProdCons/SenderAgent.java
@@ -0,0 +1,52 @@
+package Trace_ProdCons;
+
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.BaseAgent;
+import es.upv.dsic.gti_ia.core.TraceEvent;
+
+/**
+ * SenderAgent class defines the structure of a sender BaseAgent
+ * 
+ * Sends a trace event of the type "TRACE_TEST" each 1 second
+ * 
+ * @author Luis Burdalo - lburdalo@dsic.upv.es
+ */
+public class SenderAgent extends BaseAgent {
+
+	public SenderAgent(AgentID aid) throws Exception {
+		super(aid);
+	}
+
+	public void execute() {
+		TraceEvent tEvent;
+		
+		logger.info("[SENDER " + getName() +"]: Executing...");
+		
+		for (int i=0; i < 10; i++) {
+			try {
+				logger.info("[SENDER " + getName() + "]: Waiting (" + (i+1) + ")...");
+				
+				// Create a trace event of type "TRACE_TEST"
+				tEvent = new TraceEvent("TRACE_TEST", this.getAid(), "Test trace event (" + (i+1) + ")");
+
+				// Generating the trace event
+				sendTraceEvent(tEvent);
+				
+				Thread.currentThread().sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		try {
+			Thread.currentThread().sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		logger.info("[SENDER " + getName() + "]: Bye!");		
+	}
+
+}
