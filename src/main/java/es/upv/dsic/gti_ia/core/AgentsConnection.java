6c719722c3aee25c76d08cedfd584deec61b3c61
Jose Bellver <jbellver@dsic.upv.es>
2010-07-01 08:36:38 +0000

    Se deja igual que como estava en el tronco, la seguridad no afectará a esta clase, ya que las conexiones se harán individuales, y no desde aquí.

--- src/main/java/es/upv/dsic/gti_ia/core/AgentsConnection.java
+++ src/main/java/es/upv/dsic/gti_ia/core/AgentsConnection.java
@@ -12,7 +12,7 @@ import es.upv.dsic.gti_ia.organization.Configuration;
  */
 public class AgentsConnection {
 	public static org.apache.qpid.transport.Connection connection;
-	private static boolean Secure = false;
+
 	private static Configuration c = null;
 
 	/**
@@ -30,50 +30,13 @@ public class AgentsConnection {
 		connectSettings.setUsername(c.getqpidUser());
 		connectSettings.setPassword(c.getqpidPassword());
 		connectSettings.setUseSSL(c.getqpidSSL());
-		connectSettings.setSaslMechs(c.getqpidsaslMechs());
-		connectSettings.setSaslProtocol(c.getqpidsaslProtocol());
-		connectSettings.setSaslServerName(c.getqpidServerName());
-	
 
-		
 		connection.connect(connectSettings);
-	
-	//	connection.connect(c.getqpidHost(),c.getqpi, arg2, arg3, arg4, arg5, arg6)
+
 	}
 	
-	public static boolean isSecured()
-	{
-		return Secure;
-	}
-	public static void SecureConnect()
-	{
-		
-		Secure = true;
-		/*1. Validar certificado del usuario:
-		 * Primero ofrecer varios certificados en los que se confia (CA), por ahora en el del dnie (fabrica de moneda i timbre)
-		 * De que manera se nos hara llegar ese certificado, mediante ruta (mirar si puede dar problemas en rutas de fuera del host)
-		 * o mediante argumentos java.
-		 * Llamar a una funcion del agente MMS que se encarge de validar ese certificado
-		 * La funcion del MMS encargada de validar el certificador debera:
-		 * 		De alguna manera(usar sasl com hace el broker) verificar el certificado.
-		 * (o usar librerias de java, deberemos diferenciar entre las diferentes plataformas)
-		 * 		Si el resultado ok, 
-		 * 			Por cada agente que haya o vaya a crear el usuario:
-		 * 				(Modificar codigo del BaseAgent)
-		 * 				Un certificado nuevo con el nombre del agente.
-		 * 				Una conexion con el certificado anterior
-		 * 				Una session por cada conexion
-		 * 			
-		
-		
-		Pruebas verificadas:
-			Agregar el certificado de moneda y timbre a nuestra base de datos de confianza (parte del broker)
-		*/	
-		
-	}
 	
 
-	
 	
 	/**
 	 * Connects to Qpid broker taking into account all the parameters specified as input.
