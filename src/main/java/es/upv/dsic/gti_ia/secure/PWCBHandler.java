02291951efd1ec8733113ba388cc29c54b4ee79a
Jose Bellver <jbellver@dsic.upv.es>
2010-07-01 08:24:57 +0000

    Solamente asignamos la clave, no comprobamos el alias.

--- src/main/java/es/upv/dsic/gti_ia/secure/PWCBHandler.java
+++ src/main/java/es/upv/dsic/gti_ia/secure/PWCBHandler.java
@@ -32,22 +32,30 @@ public class PWCBHandler implements CallbackHandler
 		} catch (IOException e) {
 			logger.error(e);
 		}
-		String idP = prop.getProperty("alias");
+		//String idP = prop.getProperty("alias");
 		String key= prop.getProperty("key");
 		 
         for (int i = 0; i < callbacks.length; i++) {
+        	
             WSPasswordCallback pwcb = (WSPasswordCallback)callbacks[i];
+            /*
             String id = pwcb.getIdentifer();
+            System.out.println("Callbacks: "+ id);
+            
             int usage = pwcb.getUsage();
+            System.out.println("Usage "+ usage);
+            System.out.println("Decrypt "+ WSPasswordCallback.DECRYPT);
+            System.out.println("Signature "+ WSPasswordCallback.SIGNATURE);
             if (usage == WSPasswordCallback.DECRYPT ||
                 usage == WSPasswordCallback.SIGNATURE) {
-
                 // used to retrieve password for private key
-                if (idP.equals(id)) {
+               // if (idP.equals(id)) {
                     pwcb.setPassword(key);
-                }
+                //}
+                
 
-            }
+            }*/
+            pwcb.setPassword(key);
         }
     }
 }
