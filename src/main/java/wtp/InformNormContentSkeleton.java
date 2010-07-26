5f917f2fc9e534efcb90db59c772d1c43b8967f6
Elena del Val <edelval@dsic.upv.es>
2010-07-26 09:12:18 +0000

    Clases y directorios necesarios para el nuevo servicio InformNormContent.

---
+++ src/main/java/wtp/InformNormContentSkeleton.java
@@ -0,0 +1,31 @@
+
+/**
+ * InformNormContentSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.4  Built on : Apr 26, 2008 (06:24:30 EDT)
+ */
+    package wtp;
+    /**
+     *  InformNormContentSkeleton java skeleton for the axisService
+     */
+    public class InformNormContentSkeleton{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param informNormContent
+         */
+        
+                 public wtp.InformNormContentResponse InformNormContent
+                  (
+                  wtp.InformNormContent informNormContent
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#InformNormContent");
+        }
+     
+    }
+    
