b6965ba5f33b641ad017ecb688238e3aacef2f60
Juan Angel Garcia Pardo <jgarciapardo@dsic.upv.es>
2010-07-26 15:26:48 +0000

    Añadido servicio "InformNormContent" al oms.

--- src/main/java/wtp/InformNormContentSkeleton.java
+++ src/main/java/wtp/InformNormContentSkeleton.java
@@ -20,27 +20,38 @@ public class InformNormContentSkeleton
 	public wtp.InformNormContentResponse InformNormContent(
 			wtp.InformNormContent informNormContent)
 	{
+		System.out.println("Inside InformNormContent 0");
+		System.out.println("Parameters:");
+		System.out.println("AgentID="+informNormContent.getAgentID());
+		System.out.println("NormID="+informNormContent.getNormID());
+		System.out.println("-----");
 		wtp.InformNormContentResponse res = new InformNormContentResponse();
+		System.out.println("Inside InformNormContent 1");
 		res.setErrorValue("");
 		res.setStatus("Ok");
 		res.setDescription("");
-		
+		System.out.println("Inside InformNormContent 2");
 		if (informNormContent.getAgentID() == null
 				|| informNormContent.getAgentID() == ""
 				|| thomasBD.CheckExistsAgent(informNormContent.getAgentID()))
 		{
+			System.out.println("Inside InformNormContent 3");
 			res.setErrorValue("Invalid Agent ID");
 			res.setStatus("Error");
 			return res;
 		}
+		System.out.println("Inside InformNormContent 4");
 		if (thomasBD.CheckExistsNorm(informNormContent.getNormID()) == false)
 		{
+			System.out.println("Inside InformNormContent 5");
 			res.setErrorValue("Norm does not exist");
 			res.setStatus("Error");
 			return res;
 		}
+		System.out.println("Inside InformNormContent 6");
 		res.setDescription(thomasBD.GetNormContent(informNormContent
 				.getNormID()));
+		System.out.println("About to return from InformNormContent");
 		return res;
 	}
 	
