313efe20edac4008802ee5486df0382dbebaad53
Jose Bellver <jbellver@dsic.upv.es>
2010-09-10 11:24:12 +0000

    Se dejan solamente las líneas encargadas de limpiar la base de datos, las que añadían los roles y unidades propios del escenario travelagency se trasladan al proyecto de la demo del AAMAS dentro de una nueva función.

--- src/main/java/es/upv/dsic/gti_ia/organization/CleanBD.java
+++ src/main/java/es/upv/dsic/gti_ia/organization/CleanBD.java
@@ -40,31 +40,10 @@ public class CleanBD {
 			s = bd.conection.createStatement();
 			s.executeUpdate("Delete from entityplaylist");
 			s = bd.conection.createStatement();
-			s
-					.executeUpdate("Insert into unit (unitID,type,goal,parentunit) values ('travelagency','team','reservetravel',1)");
-			s = bd.conection.createStatement();
-			ResultSet rs = s
-					.executeQuery("Select * from unit where unitid='travelagency'");
-			rs.next();
-			String travelAgencyID = rs.getString("id");
-			s
-					.executeUpdate("Insert into role (roleid,accessibility,position,visibility,inheritance,unit) values ('provider','external','member','public',1,"
-							+ travelAgencyID + ")");
-			s
-					.executeUpdate("Insert into role (roleid,accessibility,position,visibility,inheritance,unit) values ('customer','external','member','public',1,"
-							+ travelAgencyID + ")");
-			s
-					.executeUpdate("Insert into role (roleid,accessibility,position,visibility,inheritance,unit) values ('payee','external','member','public',1,"
-							+ travelAgencyID + ")");
-			s
-					.executeUpdate("Insert into role (roleid,accessibility,position,visibility,inheritance,unit) values ('otro','external','member','public',1,"
-							+ travelAgencyID + ")");
-
-			s = bd.conection.createStatement();
 			s.executeUpdate("Delete from serviceprofileid");
 			s = bd.conection.createStatement();
 			s.executeUpdate("Delete from serviceprocessid");
-			s = bd.conection.createStatement();
+		
 
 
 			bd.closeConnection();
