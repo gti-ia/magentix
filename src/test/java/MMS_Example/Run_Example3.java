edf4c31e5619bbf0cca658da6a98b5e4a9612e73
Jose Bellver <jbellver@dsic.upv.es>
2011-03-23 14:00:27 +0000

    Pruebas unitarias del ejemplo 2.4

---
+++ src/test/java/MMS_Example/Run_Example3.java
@@ -0,0 +1,50 @@
+package MMS_Example;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import es.upv.dsic.gti_ia.core.AgentID;
+
+
+/**
+ * Run class is the example for changing identity.
+ *  User Alice execute the agentA, this agent sends a message to agentB (user Bob) and then
+ * changes his identity to agentC.  Next sends the message again to agentB as agentC.
+ * @author Joan Bellver - jbellver@dsic.upv.es
+ */
+public class Run_Example3 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+	/**
+	 * Setting the Logger
+	 */
+	DOMConfigurator.configure("configuration/loggin.xml");
+	Logger logger = Logger.getLogger(Run_Example3.class);
+
+	/**
+	 * Connecting to Qpid Broker
+	 */
+
+
+	try {
+
+	    /**
+	     * Instantiating a consumer agent
+	     */
+	    Agent agent = new Agent(new AgentID("agentA"));
+
+	    /**
+	     * Execute the agents
+	     */
+	    agent.start();
+
+
+	} catch (Exception e) {
+	    logger.error("Error  " + e.getMessage());
+	}
+    }
+
+}
