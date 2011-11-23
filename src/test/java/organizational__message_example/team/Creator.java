76cdb3662fb50e3760e4245fdc7cc40fe7afe81a
Jose Bellver <jbellver@dsic.upv.es>
2011-11-23 11:00:46 +0000

    Test relativo a la prueba organizacional de tipo team

---
+++ src/test/java/organizational__message_example/team/Creator.java
@@ -0,0 +1,68 @@
+package organizational__message_example.team;
+
+import es.upv.dsic.gti_ia.architecture.QueueAgent;
+import es.upv.dsic.gti_ia.architecture.FIPANames.InteractionProtocol;
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.organization.OMSProxy;
+import es.upv.dsic.gti_ia.organization.THOMASException;
+
+
+public class Creator extends QueueAgent {
+	
+	OMSProxy omsProxy = new OMSProxy(this);
+	
+	public Creator(AgentID aid) throws Exception {
+		super(aid);
+	}
+
+	public void execute() {
+
+
+
+		omsProxy.acquireRole("member", "virtual");
+		
+		
+		this.initialize_scenario();
+		
+		omsProxy.acquireRole("creador", "calculin");
+		
+		this.send_request(4, 2);
+		
+		
+		
+	}
+	
+	private void initialize_scenario()
+	{
+		omsProxy.registerUnit("calculin", "team", "unidad_calculin", "virtual");
+	
+		omsProxy.registerRole("creador", "calculin", "internal",  "member", "public","member"); 
+		omsProxy.registerRole("manager", "calculin", "internal", "member", "public","member");
+		omsProxy.registerRole("operador", "calculin", "internal", "member", "public","member");
+	
+	}
+	
+	public void send_request(int n1, int n2)
+	{
+		try {
+			//Build the organizational message
+			ACLMessage msg = omsProxy.buildOrganizationalMessage("calculin");
+			
+			//Refill the organization message with the protocol and the message content
+			msg.setPerformative(InteractionProtocol.FIPA_REQUEST);
+			msg.setProtocol(InteractionProtocol.FIPA_REQUEST);
+			msg.setLanguage("ACL");
+			msg.setContent(n1+" y "+ n2);
+
+			//Send the message
+			send(msg);
+		} catch (THOMASException e) {//Caught the thomas exception
+			
+			System.out.println("[ "+this.getName()+" ] "+ e.getContent());
+
+		}
+	}
+
+
+}
