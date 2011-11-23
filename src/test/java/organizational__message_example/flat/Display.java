5a1adda339d88275fa2906d696443c2d8194e361
Jose Bellver <jbellver@dsic.upv.es>
2011-11-23 11:00:11 +0000

    Test relativo a la prueba organizacional de tipo flat

---
+++ src/test/java/organizational__message_example/flat/Display.java
@@ -0,0 +1,72 @@
+package organizational__message_example.flat;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import es.upv.dsic.gti_ia.architecture.Monitor;
+import es.upv.dsic.gti_ia.architecture.QueueAgent;
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.organization.OMSProxy;
+
+
+public class Display extends QueueAgent {
+
+	OMSProxy omsProxy = new OMSProxy(this);
+	Monitor m = new Monitor();
+	ACLMessage mensaje_recibido = new ACLMessage();
+	private Queue<ACLMessage> messageList = new LinkedList<ACLMessage>();
+
+	public Display(AgentID aid) throws Exception {
+		super(aid);
+
+	}
+
+	public void execute() {
+
+
+
+		omsProxy.acquireRole("member", "virtual");
+		omsProxy.acquireRole("manager", "calculin");
+
+		do{
+			m.waiting();  //Waiting messages. The method onMessage is in charge of warning when a new message arrive
+			do{
+				this.displayMessage(messageList.poll());
+			}while(messageList.size() != 0);
+		}while(true);
+
+
+
+	}
+
+
+	public void displayMessage(ACLMessage _msg)
+	{
+		ACLMessage msg = _msg; 
+		ArrayList<String> roles = omsProxy.informMembers("manager","calculin");
+		
+		if (roles.contains(msg.getSender().name.toLowerCase()))//Si el agente que me envía el mensaje tiene rol manager
+			System.out.println("[ "+this.getName()+" ] Inform that "+ msg.getSender().name+" says " + msg.getContent());
+	}
+
+
+
+	public void onMessage(ACLMessage msg)
+	{
+		
+		if (msg.getReceiver().name.equals("calculin")) //Me interesan los mensajes que llegan de la organización
+		{
+			messageList.add(msg);
+			m.advise(); //Aviso de que ya tiene un nuevo mensaje
+		}
+		if (msg.getSender().name.equals("OMS") || msg.getSender().name.equals("SF"))
+			super.onMessage(msg);
+
+	}
+
+
+
+}
---
+++ src/test/java/organizational__message_example/flat/Summation.java
@@ -0,0 +1,97 @@
+package organizational__message_example.flat;
+
+import es.upv.dsic.gti_ia.architecture.FIPANames.InteractionProtocol;
+import es.upv.dsic.gti_ia.architecture.Monitor;
+import es.upv.dsic.gti_ia.architecture.QueueAgent;
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.organization.OMSProxy;
+import es.upv.dsic.gti_ia.organization.THOMASException;
+
+public class Summation extends QueueAgent {
+
+	OMSProxy omsProxy = new OMSProxy(this);
+	int result = 0;
+	int expected = 2;
+	Monitor m = new Monitor();
+
+	public Summation(AgentID aid) throws Exception {
+		super(aid);
+	}
+
+	public void execute() {
+
+		
+
+		omsProxy.acquireRole("member", "virtual");
+		omsProxy.acquireRole("manager", "calculin");
+	
+	
+		this.send_request(6,3);
+		m.waiting(10 * 1000); // Espero a que me lleguen las respuestas con timeout de 10 segundos
+		this.send_result("" + result); // Informo del resultado obtenido
+		
+		expected = 2; //Reinicio el contador, ahora solo espero uno
+		result=0;
+		this.send_request(5,3);
+		m.waiting(10 * 1000); // Espero a que me lleguen las respuestas con un timeout de 10 segundos
+		this.send_result("" + result); // Informo del resultado obtenido
+
+	}
+
+	private void add_and_advise(ACLMessage msg) {
+		result += Integer.parseInt(msg.getContent());
+		expected--;
+		if (expected == 0) {
+			m.advise(); //Aviso al hilo principal que ya tiene todas las respuestas
+			
+			
+		}
+	}
+
+	public void onMessage(ACLMessage msg) {
+
+	
+		if (msg.getSender().name.equals("agente_suma") || msg.getSender().name.contains("agente_producto")) 
+		{
+			if (!msg.getContent().contains("OK")) // Descartamos los mensajes informativos.
+			{
+				this.add_and_advise(msg);
+			}
+		}
+		if (msg.getSender().name.equals("OMS") || msg.getSender().name.equals("SF")) // Los del agente OMS y SF los volvemos a encolar, ya que sonnecesarios para el thomas proxy
+			super.onMessage(msg);
+
+	}
+
+	private void send_request(int n1, int n2) {
+		try {
+			ACLMessage msg = omsProxy.buildOrganizationalMessage("calculin");
+			msg.setPerformative(InteractionProtocol.FIPA_REQUEST);
+			msg.setProtocol(InteractionProtocol.FIPA_REQUEST);
+			msg.setLanguage("ACL");
+			msg.setContent(n1+" "+n2);
+
+			send(msg);
+		} catch (THOMASException e) {
+			System.out.println("[ " + this.getName() + " ] " + e.getContent());
+
+		}
+	}
+
+	private void send_result(String content) {
+		try {
+
+			ACLMessage msg = omsProxy.buildOrganizationalMessage("calculin");
+			msg.setPerformative(ACLMessage.INFORM);
+			msg.setLanguage("ACL");
+			msg.setContent(content);
+
+			send(msg);
+		} catch (THOMASException e) {
+			System.out.println("[ " + this.getName() + " ] " + e.getContent());
+
+		}
+	}
+
+}
