68d3788ac4cf505fb2dae42f05d28629dcc0282a
Jose Bellver <jbellver@dsic.upv.es>
2012-01-04 11:55:51 +0000

    Se añade el properties para acceder a la configuración de la base de datos.

--- src/main/java/persistence/DataBase.java
+++ src/main/java/persistence/DataBase.java
@@ -1,7 +1,12 @@
 package persistence;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+
+import configuration.Configuration;
+
 
 /**
  * Clase encargada de conexion y consultas a base de datos.
@@ -12,14 +17,25 @@ import java.sql.DriverManager;
 public class DataBase {
 	/** La conexion con la base de datos */
 	public Connection connection = null;
+	Properties properties = new Properties();
 
 	/** Se establece la conexion con la base de datos */
 	public DataBase() {
 		if (connection != null)
 			return;
 		try {
+		
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/thomas2", "root", "potallos");
+
+			properties.load(Configuration.class.getResourceAsStream("/"+"oms.properties"));
+
+
+			String serverName = properties.getProperty("serverName");
+			String mydatabase = properties.getProperty("dataBaseName");
+			String url = "jdbc:mysql://" + serverName +  "/" + mydatabase; // a JDBC url
+			String username = properties.getProperty("username");
+			String password = properties.getProperty("password");
+			connection = DriverManager.getConnection(url, username, password);
 			connection.setAutoCommit(false);
 			connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
 
