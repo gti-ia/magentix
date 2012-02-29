d4419cc28589e6fb11dce91197c3a74fff18d57a
Jose Bellver <jbellver@dsic.upv.es>
2012-02-29 10:39:06 +0000

    Se crea una nueva clase en el paquete organization debido la fusión de la rama organziation.

---
+++ src/main/java/es/upv/dsic/gti_ia/organization/THOMASException.java
@@ -0,0 +1,26 @@
+package es.upv.dsic.gti_ia.organization;
+
+/**
+ * This class represents a generic THOMASException,
+ * 
+ *
+ */
+
+public class THOMASException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String content;
+	
+	public THOMASException(String message){
+		super(message);
+		this.content = message;
+	}
+	
+	public String getContent()
+	{
+		return content;
+	}
+}
