4682ac507f466830ee2f8bde42818f1e52d40e3c
Jose Bellver <jbellver@dsic.upv.es>
2012-02-29 10:36:47 +0000

    Se crea una nueva clase en el paquete organization debido la fusión de la rama organziation.

---
+++ src/main/java/es/upv/dsic/gti_ia/organization/ResponseParser.java
@@ -0,0 +1,198 @@
+package es.upv.dsic.gti_ia.organization;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ResponseParser {
+
+
+	String serviceName="";
+	String status="";
+	String description="";
+	String specification="";
+	ArrayList<ArrayList<String>> itemsList=new ArrayList<ArrayList<String>>();
+	ArrayList<String> elementsList=new ArrayList<String>();
+	HashMap<String,String> keyAndValueList=new HashMap<String,String>();
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getSpecification() {
+		return specification;
+	}
+
+	public ArrayList<ArrayList<String>> getItemsList() {
+		return itemsList;
+	}
+
+	public ArrayList<String> getElementsList() {
+		return elementsList;
+	}
+
+	public HashMap<String,String> getKeyAndValueList() {
+		return keyAndValueList;
+	}
+
+	public String DOM2String(Document doc)
+	{
+		int coderror=0;
+		String msgerror="";
+
+		TransformerFactory transformerFactory =TransformerFactory.newInstance();
+		Transformer transformer = null;
+		try{
+			transformer = transformerFactory.newTransformer();
+		}catch (javax.xml.transform.TransformerConfigurationException error){
+			coderror=123;
+			msgerror=error.getMessage();
+			return null;
+		}
+
+		Source source = new DOMSource(doc);
+
+		StringWriter writer = new StringWriter();
+		Result result = new StreamResult(writer);
+		try{
+			transformer.transform(source,result);
+		}catch (javax.xml.transform.TransformerException error){
+			coderror=123;
+			msgerror=error.getMessage();
+			return null;
+		}
+
+		String s = writer.toString();
+		return s;
+	}
+
+	public static Document string2DOM(String s)
+	{
+		int coderror=0;
+		String msgerror="";
+
+		Document tmpX=null;
+		DocumentBuilder builder = null;
+		try{
+			builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		}catch(javax.xml.parsers.ParserConfigurationException error){
+			coderror=10;
+			msgerror="Error crando factory String2DOM "+error.getMessage();
+			return null;
+		}
+		try{
+			tmpX=builder.parse(new ByteArrayInputStream(s.getBytes()));
+		}catch(org.xml.sax.SAXException error){
+			coderror=10;
+			msgerror="Error parseo SAX String2DOM "+error.getMessage();
+			return null;
+		}catch(IOException error){
+			coderror=10;
+			msgerror="Error generando Bytes String2DOM "+error.getMessage();
+			return null;
+		}
+		return tmpX;
+	}
+
+
+	public void parseResponse(String response){
+		Document doc=string2DOM(response);
+
+		if(!doc.hasChildNodes() || !doc.getChildNodes().item(0).hasChildNodes()){
+			//incorrect specification
+			System.err.println("incorrect specification");
+			return;
+		}
+
+		itemsList=new ArrayList<ArrayList<String>>();
+		elementsList=new ArrayList<String>();
+		keyAndValueList=new HashMap<String,String>();
+		NodeList nodeList=doc.getChildNodes().item(0).getChildNodes();
+		for(int i=0;i<nodeList.getLength();i++){
+			Node n=nodeList.item(i);
+
+			if(n.getNodeName().equalsIgnoreCase("serviceName")){
+				serviceName=n.getFirstChild().getNodeValue().trim();
+			}
+			else if(n.getNodeName().equalsIgnoreCase("status")){
+				status=n.getFirstChild().getNodeValue().trim();
+			}
+			else if(n.getNodeName().equalsIgnoreCase("result")){
+				NodeList resultNodeList=n.getChildNodes();
+				for(int j=0;j<resultNodeList.getLength();j++){
+					Node resNode=resultNodeList.item(j);
+					if(resNode.getNodeName().equalsIgnoreCase("description")){
+						description=resNode.getFirstChild().getNodeValue().trim();
+					}
+					else if(resNode.getNodeName().equalsIgnoreCase("item")){
+						NodeList items=resNode.getChildNodes();
+						ArrayList<String> itemComponents=new ArrayList<String>();
+						for(int it=0;it<items.getLength();it++){
+							Node item=items.item(it);
+							if(item.getNodeType()==Node.ELEMENT_NODE)
+								itemComponents.add(item.getTextContent().trim());
+						}
+						itemsList.add(itemComponents);
+					}
+					else if(resNode.getNodeName().equalsIgnoreCase("specification")){
+						//the content is encapsulated in an XML comment <!-- -->
+						Node childN=resNode.getFirstChild();
+						specification=childN.getNodeValue().trim();
+					}
+					else if(resNode.getNodeType()==Node.ELEMENT_NODE){
+						elementsList.add(resNode.getTextContent().trim());
+					}
+
+
+				}
+			}
+			else if (n.getNodeName().equalsIgnoreCase("inputs"))
+			{
+				NodeList resultNodeList=n.getChildNodes();
+				for(int j=0;j<resultNodeList.getLength();j++){
+					{
+						Node resNode=resultNodeList.item(j);
+
+						if(resNode.getNodeType()==Node.ELEMENT_NODE)
+						{
+
+							keyAndValueList.put(resNode.getNodeName().trim(), resNode.getTextContent().trim());
+						}
+
+					}
+				}
+			}
+
+		}
+
+
+	}
+
+
+
+
+
+}
---
+++ src/main/java/es/upv/dsic/gti_ia/organization/ServiceTools.java
@@ -0,0 +1,103 @@
+package es.upv.dsic.gti_ia.organization;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import java.util.Map.Entry;
+
+public class ServiceTools {
+
+	ResponseParser rp = new ResponseParser();
+
+	/**
+	 * Builds a new string with a XML format. This XML is formed by servicename and list of the 
+	 * inputs
+	 * 
+	 * @param serviceName The name of the service
+	 * @param inputs is a HashMap formed by keys and values
+	 * @return string with a XML format.
+	 */
+	public String buildServiceContent(String serviceName, HashMap<String, String> inputs) {
+		String resultXML = "";
+
+		resultXML += "<serviceInput>\n";
+		resultXML += "<serviceName>" + serviceName + "</serviceName>\n";
+		resultXML += "<inputs>\n";
+		for (Entry<String, String> e : inputs.entrySet()) {
+			resultXML += "<" + e.getKey() + ">" + e.getValue() + "</" + e.getKey() + ">\n";
+		}
+		resultXML += "</inputs>\n";
+		resultXML += "</serviceInput>\n";
+		return resultXML;
+
+	}
+
+	/**
+	 * Parses a XML in order to extract the service name and fill the HashMap with the service inputs
+	 * 
+	 * @param string with a XML format, in this XML will be contained the service name and inputs of the service
+	 * @param inputs is a new HashMap. In this HashMap the inputs are added
+	 * @return service name
+	 */
+	public String extractServiceContent(String xml, HashMap<String, String> inputs) {
+
+		rp.parseResponse(xml);
+
+		String serviceName = rp.getServiceName();
+
+		HashMap<String, String> outputAux = rp.getKeyAndValueList();
+
+		for (Entry<String, String> e : outputAux.entrySet()) {
+
+			inputs.put(e.getKey(), e.getValue());
+		}
+
+		return serviceName;
+	}
+
+	public HashMap<String, Object> executeWebService(String serviceWSDLURL, String xmlInputs){
+		HashMap<String, String> inputs=new HashMap<String, String>();
+		this.extractServiceContent(xmlInputs, inputs);
+		return this.executeWebService(serviceWSDLURL, inputs);
+	}
+	
+	public HashMap<String, Object> executeWebService(String serviceWSDLURL, HashMap<String, String> inputs) {
+
+		Oracle oracle = new Oracle();
+		oracle.parseWSDL(serviceWSDLURL);
+
+		ArrayList<String> processInputs = oracle.getWSDLInputs();
+
+		HashMap<String, String> paramsComplete = new HashMap<String, String>();
+		Iterator<String> iterProcessInputs = processInputs.iterator();
+		while (iterProcessInputs.hasNext()) {
+			String in = iterProcessInputs.next();
+			// initialize the inputs
+			paramsComplete.put(in, "");
+		}
+
+		for (Entry<String, String> e : inputs.entrySet()) {
+
+			if (paramsComplete.get(e.getKey()) != null) {
+				paramsComplete.put(e.getKey(), e.getValue());
+			//	System.out.println("inputParamName: " + e.getKey() + " value: " + e.getValue());
+			}
+		}
+
+		// construct params list with the value of the parameters ordered...
+		ArrayList<String> params = new ArrayList<String>();
+		Iterator<String> iterInputs = processInputs.iterator();
+		while (iterInputs.hasNext()) {
+			String input = iterInputs.next();
+			params.add(paramsComplete.get(input));
+		}
+
+		ServiceClient serviceClient = new ServiceClient();
+		HashMap<String, Object> results= serviceClient.invoke(serviceWSDLURL, params);
+
+		return results;
+	}
+
+}
