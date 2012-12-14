85dfa429f7214c10f67fa8d9d9821480620f7b93
Jose Bellver <jbellver@dsic.upv.es>
2012-12-14 10:22:20 +0000

    Actualizado el fichero SQL.

--- src/main/resources/dbThomasv2.sql
+++ src/main/resources/dbThomasv2.sql
@@ -70,7 +70,7 @@ DROP TABLE IF EXISTS `thomas`.`position` ;
 
 CREATE  TABLE IF NOT EXISTS `thomas`.`position` (
   `idposition` INT NOT NULL AUTO_INCREMENT ,
-  `position` VARCHAR(45) NOT NULL ,
+  `positionName` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`idposition`) ,
   UNIQUE INDEX `position_UNIQUE` (`position` ASC) )
 ENGINE = InnoDB;
@@ -117,7 +117,7 @@ CREATE  TABLE IF NOT EXISTS `thomas`.`roleList` (
   PRIMARY KEY (`idroleList`) ,
   INDEX `fk_roleList_unitList1` (`idunitList` ASC) ,
   INDEX `fk_roleList_position1` (`idposition` ASC) ,
-  INDEX `fk_roleList_accessibility1` (`idaccessibility` ASC) ,
+  INDEX `fk_roleList_accesibility1` (`idaccessibility` ASC) ,
   INDEX `fk_roleList_visibility1` (`idvisibility` ASC) ,
   CONSTRAINT `fk_roleList_unitList1`
     FOREIGN KEY (`idunitList` )
@@ -129,7 +129,7 @@ CREATE  TABLE IF NOT EXISTS `thomas`.`roleList` (
     REFERENCES `thomas`.`position` (`idposition` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_roleList_accessibility1`
+  CONSTRAINT `fk_roleList_accesibility1`
     FOREIGN KEY (`idaccessibility` )
     REFERENCES `thomas`.`accessibility` (`idaccessibility` )
     ON DELETE NO ACTION
@@ -180,16 +180,28 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `thomas`.`deontic`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `thomas`.`deontic` ;
+
+CREATE  TABLE IF NOT EXISTS `thomas`.`deontic` (
+  `iddeontic` INT NOT NULL AUTO_INCREMENT ,
+  `deonticdesc` CHAR NULL ,
+  PRIMARY KEY (`iddeontic`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `thomas`.`targetType`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `thomas`.`targetType` ;
 
 CREATE  TABLE IF NOT EXISTS `thomas`.`targetType` (
-  `idtargetType` INT NOT NULL ,
-  `targetTableName` VARCHAR(45) NOT NULL ,
+  `idtargetType` INT NOT NULL AUTO_INCREMENT ,
+  `targetName` VARCHAR(45) NOT NULL ,
+  `targetTable` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`idtargetType`) )
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `thomas`.`actionNorm`
@@ -205,18 +217,6 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `thomas`.`deontic`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `thomas`.`deontic` ;
-
-CREATE  TABLE IF NOT EXISTS `thomas`.`deontic` (
-  `iddeontic` INT NOT NULL ,
-  `deonticdesc` CHAR NULL ,
-  PRIMARY KEY (`iddeontic`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `thomas`.`normList`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `thomas`.`normList` ;
@@ -227,28 +227,33 @@ CREATE  TABLE IF NOT EXISTS `thomas`.`normList` (
   `normName` VARCHAR(45) NOT NULL ,
   `iddeontic` INT NOT NULL ,
   `idtargetType` INT NOT NULL ,
-  `targetId` INT NOT NULL ,
+  `targetValue` INT NOT NULL ,
   `idactionnorm` INT NOT NULL ,
-  `activationSQL` TEXT NULL ,
-  `expirationSQL` TEXT NULL ,
   `normContent` TEXT NOT NULL ,
+  `normRule` TEXT NOT NULL ,
   PRIMARY KEY (`idnormList`) ,
-  INDEX `fk_normList_targetType` (`idtargetType` ASC) ,
-  INDEX `fk_normList_actionNorm` (`idactionnorm` ASC) ,
-  INDEX `fk_normlist_deontic` (`iddeontic` ASC) ,
-  CONSTRAINT `fk_normList_targetType`
+  INDEX `fk_normList_unitList1` (`idunitList` ASC) ,
+  INDEX `fk_normList_deontic1` (`iddeontic` ASC) ,
+  INDEX `fk_normList_targetType1` (`idtargetType` ASC) ,
+  INDEX `fk_normList_actionNorm1` (`idactionnorm` ASC) ,
+  CONSTRAINT `fk_normList_unitList1`
+    FOREIGN KEY (`idunitList` )
+    REFERENCES `thomas`.`unitList` (`idunitList` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_normList_deontic1`
+    FOREIGN KEY (`iddeontic` )
+    REFERENCES `thomas`.`deontic` (`iddeontic` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_normList_targetType1`
     FOREIGN KEY (`idtargetType` )
     REFERENCES `thomas`.`targetType` (`idtargetType` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_normList_actionNorm`
+  CONSTRAINT `fk_normList_actionNorm1`
     FOREIGN KEY (`idactionnorm` )
     REFERENCES `thomas`.`actionNorm` (`idactionNorm` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_normlist_deontic`
-    FOREIGN KEY (`iddeontic` )
-    REFERENCES `thomas`.`deontic` (`iddeontic` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -260,7 +265,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `thomas`.`actionNormParam` ;
 
 CREATE  TABLE IF NOT EXISTS `thomas`.`actionNormParam` (
-  `idactionNormParam` INT NOT NULL ,
+  `idactionNormParam` INT NOT NULL AUTO_INCREMENT ,
   `idnormList` INT NOT NULL ,
   `idactionNorm` INT NOT NULL ,
   `value` VARCHAR(45) NULL ,
@@ -280,10 +285,23 @@ CREATE  TABLE IF NOT EXISTS `thomas`.`actionNormParam` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `thomas`.`reservedWordList`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `thomas`.`reservedWordList` ;
+
+CREATE  TABLE IF NOT EXISTS `thomas`.`reservedWordList` (
+  `idreservedWordList` INT NOT NULL AUTO_INCREMENT ,
+  `reservedWord` VARCHAR(45) NULL ,
+  PRIMARY KEY (`idreservedWordList`) )
+ENGINE = InnoDB;
+
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
 
 -- -----------------------------------------------------
 -- Inserts unit types
@@ -357,3 +375,334 @@ VALUES
   (SELECT idposition FROM position WHERE position = 'creator'),
   (SELECT idaccessibility FROM accessibility WHERE accessibility = 'external'),
   (SELECT idvisibility FROM visibility WHERE visibility = 'public'));
+  
+-- ----------------------------------------------------
+-- Inserts default target types values:
+-- agentName, roleName, targetName
+-- -----------------------------------------------------
+
+INSERT INTO `targetType`
+(`targetName`,`targetTable`)
+VALUES
+  ('agentName','agentList');  
+  
+INSERT INTO `targetType`
+(`targetName`,`targetTable`)
+VALUES
+  ('roleName','roleList');
+  
+INSERT INTO `targetType`
+(`targetName`,`targetTable`)
+VALUES
+  ('positionName','position');
+
+-- ----------------------------------------------------
+-- Inserts default deontic values:
+-- f,o,p
+-- -----------------------------------------------------
+  
+INSERT INTO `deontic`
+(`deonticdesc`)
+VALUES
+  ('f');  
+
+  
+INSERT INTO `deontic`
+(`deonticdesc`)
+VALUES
+  ('o');  
+  
+INSERT INTO `deontic`
+(`deonticdesc`)
+VALUES
+  ('p');  
+  
+-- ----------------------------------------------------
+-- Inserts default oms actions 
+-- -----------------------------------------------------
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('registerUnit',5);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('deregisterUnit',2);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('registerRole',6);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('deregisterRole',3);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('registerNorm',3);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('deregisterNorm',3);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('allocateRole',4);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('deallocateRole',4);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('joinUnit',3);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('informAgentRole',2);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('informMembers',4);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('InformQuantityMembers',4);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('informUnit',2);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('informUnitRoles',2);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('informTargetNorms',4);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('informRole',3);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('informNorm',3);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('acquireRole',3);
+
+INSERT INTO `actionNorm`
+(`description`,`numParams`)
+VALUES ('leaveRole',3);
+
+-- ----------------------------------------------------
+-- Inserts reserved Words related with
+-- the normative language
+-- -----------------------------------------------------
+
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('flat');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('team');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('hierarchy');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('public');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('private');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('external');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('internal');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('creator');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('member');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('supervisor');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('subordinate');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('registerUnit');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('deregisterUnit');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('registerRole');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('deregisterRole');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('registerNorm');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('deregisterNorm');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('allocateRole');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('deallocateRole');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('joinUnit');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('informAgentRole');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('informMembers');
+
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('informQuantityMembers');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('informUnit');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('informUnitRoles');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('informTargetNorms');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('informRole');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('informNorm');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('acquireRole');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('leaveRole');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('isNorm');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('hasDeontic');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('hasTarget');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('hasAction');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('isRole');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('hasAccessibility');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('hasVisibility');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('hasPosition');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('isUnit');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('hasType');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('hasParent');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('div');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('mod');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('not');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('_');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('agentName');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('roleName');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('positionName');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('o');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('f');
+
+INSERT INTO `reservedWordList`
+(`reservedWord`)
+VALUES ('p');
