76f985f7d4adbc11c3c793f54b4b9c6d62651cf1
Javi Palanca <jpalanca@dsic.upv.es>
2013-04-15 11:46:05 +0000

    preparing examples jar (More Examples)

---
+++ src/examples/src/main/java/Thomas_example/Product.java
@@ -0,0 +1,195 @@
+package Thomas_example;
+
+/**
+ * In this class the agent product is represented. 
+ * Functions:
+ *  -	Acquire role operation inside the unit calculator.
+ *  - 	Register a new service square. This service is executed by the client.
+ * 	-	Register a new service product. The function of this service is to multiply the values of entry and to return the result.
+ *  - 	Provide the execution of the service product.
+ *  - 	Implements a new FIPA REQUEST protocol in order to accept the client requests for the service product.
+ *  
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import es.upv.dsic.gti_ia.cAgents.CAgent;
+import es.upv.dsic.gti_ia.cAgents.CFactory;
+import es.upv.dsic.gti_ia.cAgents.CProcessor;
+import es.upv.dsic.gti_ia.cAgents.protocols.FIPA_REQUEST_Participant;
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.organization.OMSProxy;
+import es.upv.dsic.gti_ia.organization.SFProxy;
+import es.upv.dsic.gti_ia.organization.ServiceTools;
+import es.upv.dsic.gti_ia.organization.exception.THOMASException;
+
+public class Product extends CAgent {
+
+	OMSProxy omsProxy = new OMSProxy(this);
+	SFProxy sfProxy = new SFProxy(this);
+
+	public Product(AgentID aid) throws Exception {
+		super(aid);
+
+	}
+
+	protected void execution(CProcessor myProcessor, ACLMessage welcomeMessage) {
+
+		try {
+
+			CFactory informTalk = new myInform_Protocol().newFactory("Inform_TALK", 1, myProcessor.getMyAgent());
+			this.addFactoryAsParticipant(informTalk);
+
+			String result = omsProxy.acquireRole("operation", "calculator");
+			logger.info("[" + this.getName() + "] Result acquire role operation: " + result);
+
+			System.out.println("[" + this.getName() + "]" + " operation (calculator) role acquired");
+
+			ArrayList<String> resultRegister = sfProxy
+					.registerService("http://localhost:8080/testSFservices/testSFservices/owl/owls/Product.owl");
+			Iterator<String> iterRes = resultRegister.iterator();
+			String registerRes = "";
+			while (iterRes.hasNext()) {
+				registerRes += iterRes.next() + "\n";
+			}
+			logger.info("[" + this.getName() + "] Result registerService: " + registerRes);
+
+			resultRegister = sfProxy
+					.registerService("http://localhost:8080/testSFservices/testSFservices/owl/owls/Square.owl");
+			iterRes = resultRegister.iterator();
+			registerRes = "";
+			while (iterRes.hasNext()) {
+				registerRes += iterRes.next() + "\n";
+			}
+			logger.info("[" + this.getName() + "] Result registerService: " + registerRes);
+
+			System.out.println("[" + this.getName() + "] " + "Product and Square services registered. Waiting Request");
+
+			CFactory additionTalk = new myFIPA_REQUEST().newFactory("PRODUCT_TALK", null, 0, myProcessor.getMyAgent());
+
+			this.addFactoryAsParticipant(additionTalk);
+
+		} catch (THOMASException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	protected void finalize(CProcessor firstProcessor, ACLMessage finalizeMessage) {
+		System.out.println("[" + firstProcessor.getMyAgent().getName() + "] End execution");
+	}
+
+	// ------------------------------------------------------------------------
+	// -----------------------CFactory implementation--------------------------
+	// ------------------------------------------------------------------------
+
+	class myFIPA_REQUEST extends FIPA_REQUEST_Participant {
+
+		ServiceTools st = new ServiceTools();
+		HashMap<String, String> inputs = new HashMap<String, String>();
+		String serviceName = "";
+
+		@Override
+		protected String doAction(CProcessor myProcessor) {
+			String next = "";
+
+			double result = 1;
+			try {
+
+				for (Entry<String, String> e : inputs.entrySet()) {
+
+					result *= Double.parseDouble(e.getValue());
+				}
+
+				next = "INFORM";
+				String resultXML = "";
+				resultXML += "<serviceOutput>\n";
+				resultXML += "<serviceName>" + serviceName + "</serviceName>\n";
+				resultXML += "<outputs>\n";
+				resultXML += "<Result>" + result + "</Result>\n";
+				resultXML += "</outputs>\n";
+				resultXML += "</serviceOutput>\n";
+
+				myProcessor.getLastReceivedMessage().setContent(resultXML);
+
+			} catch (Exception e) {
+				next = "FAILURE";
+			}
+
+			return next;
+		}
+
+		@Override
+		protected void doInform(CProcessor myProcessor, ACLMessage response) {
+			ACLMessage lastReceivedMessage = myProcessor.getLastReceivedMessage();
+			response.setContent(lastReceivedMessage.getContent());
+		}
+
+		@Override
+		protected String doReceiveRequest(CProcessor myProcessor, ACLMessage request) {
+			String next = "";
+			ACLMessage msg = request;
+
+			if (msg != null) {
+
+				try {
+
+					inputs.clear();
+					serviceName = st.extractServiceContent(msg.getContent(), inputs);
+
+					if (serviceName.toLowerCase().contains("product")) {
+
+						logger.info("AGREE");
+						next = "AGREE";
+
+					} else {
+
+						logger.info("REFUSE");
+						next = "REFUSE";
+					}
+
+				} catch (Exception e) {
+
+					logger.info("EXCEPTION");
+					System.out.println(e);
+					e.printStackTrace();
+					throw new RuntimeException(e.getMessage());
+
+				}
+
+			} else {
+
+				logger.info("NOTUNDERSTOOD");
+				next = "NOT_UNDERSTOOD";
+			}
+
+			logger.info("[Product]Sending First message:" + next);
+
+			return next;
+		}
+	}
+
+	class myInform_Protocol extends ExampleEndedInform {
+
+		@Override
+		protected boolean doFinish(CProcessor myProcessor, ACLMessage msgReceived) {
+			if (msgReceived.getHeaderValue("EXAMPLEENDED") != null)
+				return true;
+			else
+				return false;
+		}
+
+		@Override
+		protected void doDie(CProcessor myProcessor) {
+			myProcessor.ShutdownAgent();
+
+		}
+
+	}
+
+}
---
+++ src/examples/src/main/java/TraceTest_1/Publisher.java
@@ -0,0 +1,80 @@
+package TraceTest_1;
+
+import java.lang.System;
+
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.BaseAgent;
+import es.upv.dsic.gti_ia.core.TraceEvent;
+import es.upv.dsic.gti_ia.trace.TraceInteract;
+
+/*****************************************************************************************
+/*                                      TraceTest_1                                      *
+/*****************************************************************************************
+/*                     Author: Luis Burdalo (lburdalo@dsic.upv.es)                       *
+/*****************************************************************************************
+/*                                     DESCRIPTION                                       *
+/*****************************************************************************************
+    Simple test with two agents: a PUBLISHER agent and a SUBSCRIBER agent.
+    
+    The PUBLISHER agent publishes a tracing service called 'DD_Test_TS' which content is
+    just a string. Each second, the publisher generates the trace event. After 10
+    seconds, the PUBLISHER unpublishes the tracing service and says 'Bye!'. 
+*****************************************************************************************/
+public class Publisher extends BaseAgent {
+	private final int N_EVENTS = 10;
+	
+	public Publisher(AgentID aid) throws Exception {
+		super(aid);
+		/**
+		 * Initializing tracing services and stuff
+		 */
+		System.out.println("[PUBLISHER]: Basic test start...");
+		
+		System.out.println("[PUBLISHER]: Publishing tracing service:");
+		TraceInteract.publishTracingService(this, "DD_Test_TS", "Domain Dependent Test Tracing Service");
+		System.out.println("[PUBLISHER]: Done!");
+	}
+
+	public void execute() {
+		TraceEvent tEvent;
+		int i;
+		
+		System.out.println("[PUBLISHER]: Sending trace events");
+		for (i=0; i < N_EVENTS; i++) {
+			try {
+				tEvent = new TraceEvent("DD_Test_TS", this.getAid(), "Event " + (i+1) + " of " + N_EVENTS);
+				// Generating the trace event
+				sendTraceEvent(tEvent);
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+
+		System.out.println("[PUBLISHER]: Now unpublishing tracing service:");
+		TraceInteract.unpublishTracingService(this, "DD_Test_TS");
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("[PUBLISHER]: Bye!");
+	}
+	
+	public void onTraceEvent(TraceEvent tEvent) {
+		/**
+		 * When a trace event arrives, it prints it on the screen
+		 */
+		System.out.println("[PUBLISHER]: Received from " + tEvent.getOriginEntity().getAid().name + ": " + tEvent.getContent());
+	}
+	
+	public void onMessage(ACLMessage msg){
+		System.out.println("[PUBLISHER]: Received from " + msg.getSender().name + ": [ " + msg.getPerformative() + " " + msg.getContent() + " ]");
+	}
+}
