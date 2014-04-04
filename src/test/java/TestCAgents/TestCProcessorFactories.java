98164a2371c2c97ee9106f9c40856b598c3f6b00
Javier Jorge <jjorge@dsic.upv.es>
2014-04-04 08:16:34 +0000

    Debugging: Unfinished test on TestCAgents

--- src/test/java/TestCAgents/TestCProcessorFactories.java
+++ src/test/java/TestCAgents/TestCProcessorFactories.java
@@ -21,101 +21,101 @@ import es.upv.dsic.gti_ia.core.AgentsConnection;
  */
 
 public class TestCProcessorFactories extends TestCase {
-
-	HarryClass Harry;
-	SallyClass Sally;
-	Process qpid_broker;
-
-	public TestCProcessorFactories(String name) {
-		super(name);
-
-	}
-	@Before
-	public void setUp() throws Exception {
-		super.setUp();
-		qpid_broker = qpidManager.UnixQpidManager.startQpid(Runtime.getRuntime(), qpid_broker);
-		try {
-
-			/**
-			 * Setting the configuration
-			 */
-			DOMConfigurator.configure("configuration/loggin.xml");
-
-			/**
-			 * Connecting to Qpid Broker, default localhost.
-			 */
-			AgentsConnection.connect();
-
-			/**
-			 * Instantiating the CAgents
-			 */
-			Harry = new HarryClass(new AgentID("Harry"));
-			Sally = new SallyClass(new AgentID("Sally"));
-
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-
-	}
-
-	/**
-	 * Testing propose message sent by the initiator factory in Harry
-	 */
-	@Test(timeout = 30000)
-	public void testProposeMessage() {
-		Sally.start();
-		// Temporal wait for resolving issues
-		/*
-		 * try { Thread.sleep(3000); } catch (InterruptedException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); }
-		 */
-		Harry.start();
-
-		// If Agent has not received the message
-		while (Sally.receivedMsg.equalsIgnoreCase("")) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		assertEquals("PROPOSE: Will you come with me to a movie?",
-				Sally.receivedMsg);
-	}
-
-	/**
-	 * Testing refuse message sent by the participant factory in Sally
-	 */
-	@Test(timeout = 30000)
-	public void testRefuseMessage() {
-		System.out.println("SEGUNDO TEST");
-
-		Sally.start();
-		// Temporal wait to resolve issues
-		/*
-		 * try { Thread.sleep(3000); } catch (InterruptedException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); }
-		 */
-		Harry.start();
-
-		// If Agent has not received the message
-		while (Harry.receivedMsg.equalsIgnoreCase("")) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		assertEquals("REFUSE: Maybe someday", Harry.receivedMsg);
-	}
-	@After
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		AgentsConnection.disconnect();
-		qpidManager.UnixQpidManager.stopQpid(qpid_broker);
-	}
+//
+//	HarryClass Harry;
+//	SallyClass Sally;
+//	Process qpid_broker;
+//
+//	public TestCProcessorFactories(String name) {
+//		super(name);
+//
+//	}
+//	@Before
+//	public void setUp() throws Exception {
+//		super.setUp();
+//		qpid_broker = qpidManager.UnixQpidManager.startQpid(Runtime.getRuntime(), qpid_broker);
+//		try {
+//
+//			/**
+//			 * Setting the configuration
+//			 */
+//			DOMConfigurator.configure("configuration/loggin.xml");
+//
+//			/**
+//			 * Connecting to Qpid Broker, default localhost.
+//			 */
+//			AgentsConnection.connect();
+//
+//			/**
+//			 * Instantiating the CAgents
+//			 */
+//			Harry = new HarryClass(new AgentID("Harry"));
+//			Sally = new SallyClass(new AgentID("Sally"));
+//
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//
+//	}
+//
+//	/**
+//	 * Testing propose message sent by the initiator factory in Harry
+//	 */
+//	@Test(timeout = 30000)
+//	public void testProposeMessage() {
+//		Sally.start();
+//		// Temporal wait for resolving issues
+//		/*
+//		 * try { Thread.sleep(3000); } catch (InterruptedException e) { // TODO
+//		 * Auto-generated catch block e.printStackTrace(); }
+//		 */
+//		Harry.start();
+//
+//		// If Agent has not received the message
+//		while (Sally.receivedMsg.equalsIgnoreCase("")) {
+//			try {
+//				Thread.sleep(100);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//
+//		assertEquals("PROPOSE: Will you come with me to a movie?",
+//				Sally.receivedMsg);
+//	}
+//
+//	/**
+//	 * Testing refuse message sent by the participant factory in Sally
+//	 */
+//	@Test(timeout = 30000)
+//	public void testRefuseMessage() {
+//		System.out.println("SEGUNDO TEST");
+//
+//		Sally.start();
+//		// Temporal wait to resolve issues
+//		/*
+//		 * try { Thread.sleep(3000); } catch (InterruptedException e) { // TODO
+//		 * Auto-generated catch block e.printStackTrace(); }
+//		 */
+//		Harry.start();
+//
+//		// If Agent has not received the message
+//		while (Harry.receivedMsg.equalsIgnoreCase("")) {
+//			try {
+//				Thread.sleep(100);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//
+//		assertEquals("REFUSE: Maybe someday", Harry.receivedMsg);
+//	}
+//	@After
+//	protected void tearDown() throws Exception {
+//		super.tearDown();
+//		AgentsConnection.disconnect();
+//		qpidManager.UnixQpidManager.stopQpid(qpid_broker);
+//	}
 }
