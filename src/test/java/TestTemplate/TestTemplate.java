a1b6217d0fe297bb16667d0fe902f48d830de43a
Cristian Peñaranda <cpenaranda@dsic.upv.es>
2014-06-27 08:22:23 +0000

    Migrated TestTemplate/ tests to JUnit4

--- src/test/java/TestTemplate/TestTemplate.java
+++ src/test/java/TestTemplate/TestTemplate.java
@@ -1,16 +1,20 @@
 package TestTemplate;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import es.upv.dsic.gti_ia.core.ACLMessage;
 import es.upv.dsic.gti_ia.core.MessageFilter;
 
-import junit.framework.TestCase;
-
-public class TestTemplate extends TestCase {
+public class TestTemplate {
 
 	MessageFilter template = null;
 	ACLMessage msg = null;
-
-	protected void setUp() throws Exception {
+	
+	@Before
+	public void setUp() throws Exception {
 
 		template = new MessageFilter(
 				"performative = UNKNOWN AND purpose= vender");
@@ -23,7 +27,7 @@ public class TestTemplate extends TestCase {
 		msg.setHeader("place", "mercado");
 
 	}
-
+	@Test(timeout = 5 * 1000)
 	public void testTemplate() {
 
 		if (!template.compareHeaders(msg))
